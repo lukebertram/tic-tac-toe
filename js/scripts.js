233febafb0e6329c11a1d3c58a93f209010cf820
Luke Bertram and Zachary Marston <luke@>
2017-11-02 11:38:24 -0700

    Add tracking of marked squares in spaces array

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 //back-end business town
 function Game(p1,p2){
-  this.player1 = p1;
-  this.player2 = p2;
+  this.player1 = new Player("X",p1);
+  this.player2 = new Player("O",p2);
   this.board = new Board();
   this.turn = true;
+}
+
+Game.prototype.currentPlayer = function(){
+  if (this.turn){
+    return this.player1;
+  } else {
+    return this.player2;
+  }
 }
 
 function Player(team, name){
@@ -21,7 +29,7 @@ function Board(){
 }
 
 function Space(xCoord, yCoord){
-  this.coords = xCoord.toString() + yCoord.toString();
+  this.name = xCoord.toString() + ", " + yCoord.toString();
   this.xCoord = xCoord;
   this.yCoord = yCoord;
   this.markedBy = "";
@@ -30,6 +38,17 @@ function Space(xCoord, yCoord){
 var generateClickHandler = function(xCoord, yCoord) {
   return function() {
     $('div#col'+ xCoord +'-'+ yCoord).css('background-color', 'red');
+    debugger
+    var spaceIndex;
+    if (xCoord === 1){
+      spaceIndex = yCoord -1;
+    } else if (xCoord === 2){
+      spaceIndex = yCoord + 2;
+    } else {
+      spaceIndex = yCoord + 5;
+    }
+    var team = game.currentPlayer().team;
+    game.board.spaces[spaceIndex].markedBy = team;
   }
 }
 
@@ -39,9 +58,9 @@ var board = game.board;
 $(document).ready(function(){
   for (var x = 1; x < 4; x++) {
     for (var y = 1; y < 4; y++) {
-      var xCoord = x.toString();
-      var yCoord = y.toString();
-      $('div#col'+ xCoord +'-'+ yCoord).click(generateClickHandler(xCoord, yCoord));
+      // var xCoord = x.toString();
+      // var yCoord = y.toString();
+      $('div#col'+ x +'-'+ y).click(generateClickHandler(x, y));
         // for (var i = 0; i < spaces.length; i++) {
         //   if (spaces[i].coords === (x.toString + y.toString)){
         //     spaces[i].markedBy = "X";
